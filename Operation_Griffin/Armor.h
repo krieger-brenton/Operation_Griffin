644cb9b6dba37922e6ff3717bb194471f3fb0099
BrentKrieger <krieger.brenton@gmail.com>
2014-11-07 12:30:10 -0700

    Comments added

--- Operation_Griffin/Armor.h
+++ Operation_Griffin/Armor.h
@@ -1,3 +1,39 @@
+/******************************************************************************
+* CLASS ARMOR
+* Member Varaibles:
+* 	int type - The part of the body were the Armor can be equiped. (e.g., Head,
+		Chest, Legs)
+*	int material - The material the Armor is made of. (e.g., Cloth, Leather, 
+		Metal)
+*	int resistance - Statistical value to calculate how much damage the Armor
+		can negate.
+*
+* Contructors:
+* default constructor:
+* Armor() - Sets everything to zero or NO_TYPE/NO_MAT
+* Other constructors:
+* Armor(int,int,int) - calls the parent class, Item's default constructor then
+*	sets Armor's member variables to the parameters using the class's mutators
+* constructor (int,int,int,int,int,int) - calls Item's non-default constructor
+*	and then sets Armor's member variables to the parameters using the class's
+*	mutator functions.
+*
+* Other Functions:
+*	Accessors: simply returns the integer values for each member variable.
+*	void getType()
+*	void getMaterial() 
+*	void getResistance()
+*	Mutators:
+*	void setType(int pType) - If pType is not within 0 and 3(NO_TYPE) it is
+		set to 3(NO_TYPE) and an error is displayed.
+*	void setMaterial(int pMaterial) - If pMaterial is not within 0 and 3(NO_MAT)
+		it is set to 3(NO_MAT) and an error is displayed.
+*	void setResistance(int pResistance) - If pResistance is negative then 
+		resistance is set to zero and an error is displayed.
+*	void print() - Virtual function, overwritten to show all member and 
+		inherited variables.
+******************************************************************************/
+
 #ifndef ARMOR_H
 #define ARMOR_H
 
@@ -10,9 +46,10 @@ class Armor : public Item
 {
 public:
 	Armor() : Item(), type(NO_TYPE), material(NO_MAT), resistance(0) { setName("ARMOR"); }
+	Armor(int pType, int pMaterial, int pResistance);
 	Armor(int pType, int pMaterial, int pResistance, int pStrength,
 		int pIntelligence, int pAgility);
-	Armor(int pType, int pMaterial, int pResistance);
+	
 
 	void setType(int pType);
 	void setMaterial(int pMaterial);
@@ -29,13 +66,5 @@ private:
 	int material;
 	int resistance;
 };
-
-
-
-
-
-
-
-
 
 #endif /* ARMOR_H */
